a9f9e0b207dc9a9b3ffcc19fc10390964eddb225
yaok <yaokouassi321@gmail.com>
2025-11-15 17:24:37 +0100

    primi file per MVC

---
+++ lib/models/med.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Med extends StatelessWidget {
+  const Med({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -11,14 +11,6 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
 
-  final List<City> _cities = [
-    City(id: 1, idRegione: 1, popolazione: 160000, nome: 'Perugia', provincia: 'PG'),
-    City(id: 2, idRegione: 1, popolazione: 60000, nome: 'Narni', provincia: 'TR'),
-    City(id: 3, idRegione: 1, popolazione: 40000, nome: 'Assisi', provincia: 'PG'),
-    City(id: 4, idRegione: 1, popolazione: 140000, nome: 'Terni', provincia: 'TR'),
-    City(id: 5, idRegione: 1, popolazione: 25000, nome: 'Umbertide', provincia: 'PG'),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,13 +25,12 @@ class _HomepageState extends State<Homepage> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-          child: ListView.builder(
-            itemCount: _cities.length,
-            itemBuilder: (context, index) {
-              City currentCity = _cities[index];
-              return CityCard(currentCity: currentCity);
-            },
-          ),
+          child: Placeholder()
+          // child: ListView.builder(
+          //   itemCount: _cities.length,
+          //   itemBuilder: (context, index) {}
+              
+          // ),
         ),
       )
     );
